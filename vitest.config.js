4b53b5be34145e7ed80b81b4bfcdaa3f73493598
zoedsoupe <zoey.spessanha@dashbit.co>
2025-08-26 14:51:35 -0300

    chore: correctly uses vitest on ci

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
   test: {
-    exclude: ['.bun', 'node_modules', 'dist'],
+    exclude: ['.bun', 'node_modules', 'dist', '.direnv'],
   },
 });
