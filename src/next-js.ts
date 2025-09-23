b73f5a5cb0ece6557d65d5855891850f476bf304
zoedsoupe <zoey.spessanha@dashbit.co>
2025-09-23 12:32:22 -0300

    remove next-connect dependency

--- src/next-js.ts
+++ src/next-js.ts
@@ -1,6 +1,11 @@
-import { expressWrapper } from 'next-connect';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { checkSecurity, methodNotAllowed, type TidewaveConfig } from './http';
+import {
+  checkSecurity,
+  methodNotAllowed,
+  type TidewaveConfig,
+  type Request,
+  type Response,
+} from './http';
 import { handleMcp } from './http/handlers/mcp';
 import { handleShell } from './http/handlers/shell';
 import bodyParser from 'body-parser';
@@ -12,16 +17,45 @@ const DEFAULT_CONFIG: TidewaveConfig = {
   host: 'localhost',
 };
 
-type NextHandler = (_req: NextApiRequest, _res: NextApiResponse) => Promise<void>;
+type NextJsHandler = (_req: NextApiRequest, _res: NextApiResponse) => Promise<void>;
 
-export function toNodeHandler(config: TidewaveConfig = DEFAULT_CONFIG): NextHandler {
+type NextHandler = () => ValueOrPromise<unknown>;
+
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+export type FunctionLike = (...args: any[]) => unknown;
+
+export type Nextable<H extends FunctionLike> = (
+  ...args: [...Parameters<H>, NextHandler]
+) => ValueOrPromise<any>; /* eslint-disable-line @typescript-eslint/no-explicit-any */
+
+export type ValueOrPromise<T> = T | Promise<T>;
+
+type NextFunction = (err?: unknown) => void;
+
+type ExpressRequestHandler<Req, Res> = (req: Req, res: Res, next: NextFunction) => void;
+
+export type RequestHandler<Req extends Request, Res extends Response> = (
+  req: Req,
+  res: Res,
+) => ValueOrPromise<void>;
+
+export function connectWrapper<Req extends Request, Res extends Response>(
+  fn: ExpressRequestHandler<Req, Res>,
+): Nextable<RequestHandler<Req, Res>> {
+  return (req, res, next) =>
+    new Promise<void>((resolve, reject) => {
+      fn(req, res, err => (err ? reject(err) : resolve()));
+    }).then(next);
+}
+
+export function toNodeHandler(config: TidewaveConfig = DEFAULT_CONFIG): NextJsHandler {
   // we don't need any `next` request handler
   const next: () => void = () => {};
 
   return async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
     const securityMiddleware = checkSecurity(config);
-    await expressWrapper(securityMiddleware)(req, res, next);
-    await expressWrapper(bodyParser.json())(req, res, next);
+    await connectWrapper(securityMiddleware)(req, res, next);
+    await connectWrapper(bodyParser.json())(req, res, next);
 
     const path = req.query.path as string[];
     const endpoint = path?.[0];
@@ -30,9 +64,9 @@ export function toNodeHandler(config: TidewaveConfig = DEFAULT_CONFIG): NextHand
       return methodNotAllowed(res);
     }
 
-    if (endpoint === 'mcp') return handleMcp(req, res, next);
-    if (endpoint === 'shell') return handleShell(req, res, next);
+    if (endpoint === 'mcp') return connectWrapper(handleMcp)(req, res, next);
+    if (endpoint === 'shell') return connectWrapper(handleShell)(req, res, next);
 
-    res.status(404).json({ message: `Route not found: ${req.method} ${req.url}` });
+    return res.status(404).json({ message: `Route not found: ${req.method} ${req.url}` });
   };
 }
