512389899946551a37cd189000f5e2463990dc03
zoedsoupe <zoey.spessanha@dashbit.co>
2025-10-06 09:09:16 -0300

    fix: remove dynamic api header access

--- src/next-js.ts
+++ src/next-js.ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse, type NextRequest } from 'next/server';
-import { headers } from 'next/headers';
 import { checkSecurity, HANDLERS, methodNotAllowed, type Request, type Response } from './http';
 import bodyParser from 'body-parser';
 import type { TidewaveConfig } from './core';
@@ -55,22 +54,22 @@ export async function tidewaveHandler(
     );
   }
 
-  const origin = (await headers()).get('host');
-
-  if (origin) {
-    const [hostname, port] = origin.split(':');
-    config.host = hostname ? hostname : config.host;
-    config.port = port ? Number(port) : config.port;
-  }
-
   return async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+    const origin = req.headers.host;
+
+    if (origin) {
+      const [hostname, port] = origin.split(':');
+      config.host = hostname ? hostname : config.host;
+      config.port = port ? Number(port) : config.port;
+    }
+
     const next: () => void = () => {};
     const securityMiddleware = checkSecurity(config);
     await connectWrapper(securityMiddleware)(req, res, next);
     await connectWrapper(bodyParser.json())(req, res, next);
 
     // Parse endpoint manually, rewrite doesn't populate query
-    const url = new URL(req.url ?? '', `http://${req.headers.host}`);
+    const url = new URL(req.url ?? '', `http://${origin}`);
     const segments = url.pathname.split('/').filter(Boolean);
     const [_tidewave, endpoint] = segments;
 
