384e5b1c32f09b48424ee031f9fae5888a4e7a90
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-11-13 18:26:38 +0100

    Ensure project_eval result is received by the parent (#45)
    Currently the child does `process.send` and immediately `process.exit`.
    My understanding is that `process.send` is async and it is not
    guaranteed that the message is sent by the time the child exists. To
    guarantee it, we make the parent send an acknowledgement before exiting.
    See https://github.com/nodejs/node/issues/6767.

--- src/evaluation/eval_worker.ts
+++ src/evaluation/eval_worker.ts
@@ -1,28 +1,38 @@
 import type { EvaluationRequest } from '../core';
 
-process.on('message', async ({ code, args }: EvaluationRequest) => {
+type WorkerMessage = { type: 'evaluate'; request: EvaluationRequest } | { type: 'finish' };
+
+process.on('message', async (message: WorkerMessage) => {
   if (!process.send) {
     console.error('[Tidewave] Unable to establish communication channel with code-executor.');
     process.exit(1);
   }
 
-  try {
-    const AsyncFunction = Object.getPrototypeOf(async () => {}).constructor;
-    const fn = new AsyncFunction(code);
-    const result = await fn(...args);
+  if (message.type === 'evaluate') {
+    const { code, args } = message.request;
 
-    process.send({
-      type: 'result',
-      success: true,
-      data: (result ?? null) && result,
-    });
-  } catch (error) {
-    process.send({
-      type: 'result',
-      success: false,
-      data: new String(error),
-    });
+    try {
+      const AsyncFunction = Object.getPrototypeOf(async () => {}).constructor;
+      const fn = new AsyncFunction(code);
+      const result = await fn(...args);
+
+      process.send({
+        type: 'result',
+        success: true,
+        data: (result ?? null) && result,
+      });
+    } catch (error) {
+      process.send({
+        type: 'result',
+        success: false,
+        data: new String(error),
+      });
+    }
+  } else if (message.type === 'finish') {
+    // Note that process.send is async [1], so we wait for the parent
+    // to receive the result and then tell us to exit.
+    //
+    // [1]: https://github.com/nodejs/node/issues/6767
+    process.exit(0);
   }
-
-  process.exit(0);
 });
